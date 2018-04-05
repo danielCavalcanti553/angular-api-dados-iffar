fa39fac27d8f906be793138c6796a461e9f61528
Daniel <daniel.cavalcanti@outlook.com.br>
2018-04-04 21:37:16 -0300

    primeiro commit

---
+++ src/app/aluno/aluno.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Aluno } from '../domain/aluno';
+import { AlunoService } from '../services/aluno.service';
+
+@Component({
+  selector: 'app-aluno',
+  templateUrl: './aluno.component.html',
+  styleUrls: ['./aluno.component.css']
+})
+export class AlunoComponent implements OnInit {
+
+
+  alunos : Aluno[]; // armazena os contratos do servidor remoto
+
+  constructor(private alunoService : AlunoService) {
+    this.getAlunos();
+   }
+
+  ngOnInit() {
+    
+  }
+
+  getAlunos(){
+    this.alunoService.getAlunos()
+      .subscribe(response => {
+        this.alunos = response['data'];
+        console.log(response['data']);
+      })
+  }
+
+}
